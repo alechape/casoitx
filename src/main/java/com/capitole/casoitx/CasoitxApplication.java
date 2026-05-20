5b2e4247eed0e5d665623ca7cebf0b9882f002b5
Alexander Echavarria <alex.echavarria.perez@outlook.com>
2026-05-19 22:29:10 -0500

    Initial commit

---
+++ src/main/java/com/capitole/casoitx/CasoitxApplication.java
@@ -0,0 +1,13 @@
+package com.capitole.casoitx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoitxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoitxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capitole/casoitx/CasoitxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.casoitx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoitxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
